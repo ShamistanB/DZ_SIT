04711fcdbfa555eab826bd528138fa8dc5eb6a97
unknown <shamistan2002@icloud.com>
2022-04-27 02:40:44 +0400

    fib_3

--- DZ.cpp
+++ DZ.cpp
@@ -5,17 +5,19 @@ int fib(int n)
 {
     int c, a = 0;
     int b = 1;
+    cout << a << endl << b << endl;
     for(int i = 3; i <= n; i++)
     {
         c = b;
         b = a + b;
         a = c;
+        cout << b << endl;
     }
     return b;
 }
 
 int main()
 {
-    cout << fib(10) << endl;
+    fib(10);
     return 0;
 }
